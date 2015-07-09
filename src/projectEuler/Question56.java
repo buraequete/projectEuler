0b5e1b5f9bd404ba90945edaf82f66ec01b6322a
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2015-07-09 14:43:18 +0900

    bigdecimal native func used

--- src/projectEuler/Question56.java
+++ src/projectEuler/Question56.java
@@ -12,24 +12,14 @@ public class Question56 {
         TimeHelper.start();
         for (int i = 1; i < 100; i++) {
             for (int j = 1; j < 100; j++) {
-                int now = getDigitSum(Bigpower(i, j));
+                int now = getDigitSum(new BigDecimal(i).pow(j));
                 if (now > max) {
                     max = now;
                 }
             }
         }
-
         TimeHelper.stop();
         ResultHelper.printOut(max);
-    }
-
-    private static BigDecimal Bigpower(int x, int y) {
-        BigDecimal sum = BigDecimal.ONE, bigX = new BigDecimal(x);
-        for (int i = 0; i < y; i++) {
-            sum = sum.multiply(bigX);
-        }
-
-        return sum;
     }
 
     private static int getDigitSum(BigDecimal num) {

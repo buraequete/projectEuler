32753bcca6f30bc2a0bcc50217c189691c54f6ac
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2017-05-17 12:16:18 +0900

    some refactor

---
+++ src/question/Question69.java
@@ -0,0 +1,19 @@
+package question;
+
+import helper.ResultHelper;
+import helper.TimeHelper;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Question69 {
+	public static void main(String[] args) {
+		TimeHelper.start();
+		ResultHelper.printOut("");
+		TimeHelper.stop();
+	}
+}

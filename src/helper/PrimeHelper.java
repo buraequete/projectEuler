44880ed76b8661539c9252a10c4b73ad1bf06e57
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2017-05-19 16:30:03 +0900

    reinvent totient function

--- src/helper/PrimeHelper.java
+++ src/helper/PrimeHelper.java
@@ -23,24 +23,11 @@ public class PrimeHelper {
 		primeList = Arrays.asList(primes);
 	}
 
-	public static boolean isCoprime(int small, int large) {
-		if (large % small == 0 || (small % 2 == 0 && large % 2 == 0) || (small % 3 == 0 && large % 3 == 0) || (small % 5 == 0 && large % 5 == 0)) {
-			return false;
-		} else if (small > 2) {
-			for (Integer prime : getPrimes(small - 1)) {
-				if (small % prime == 0 && large % prime == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
 	public static boolean isPrime(int n) {
 		return primeList.get(n);
 	}
 
 	public static List<Integer> getPrimes(int limit) {
-		return IntStream.rangeClosed(1, limit).boxed().filter(p -> primeList.get(p)).collect(Collectors.toList());
+		return IntStream.range(1, limit).boxed().filter(p -> primeList.get(p)).collect(Collectors.toList());
 	}
 }
--- src/question/Question69.java
+++ src/question/Question69.java
@@ -1,38 +1,60 @@
 package question;
 
-import helper.PrimeHelper;
 import helper.ResultHelper;
 import helper.TimeHelper;
-import java.util.stream.IntStream;
 
 public class Question69 {
-	static double max = -1;
+	static int input = (int) 1e6;
 	static int result = -1;
+	static double max = -1;
+	static int phi[] = new int[input + 1];
 
-	// After finding that for n < 100, max n/φ(n) was n = 30, and for n < 1000, it was 210.
-	// From there I was able to deduce that the values were a series of multiplication of prime numbers,
-	// thus the result was 2*3*5*7*11*13*17
-	// (!) This program is highly inefficient
 	public static void main(String[] args) {
 		TimeHelper.start();
-		iterate(1000);
+		totientiate(input);
 		ResultHelper.printOut("Result is " + result);
 		TimeHelper.stop();
 	}
 
-	static void iterate(int limit) {
-		PrimeHelper.fillPrimes(limit);
-		IntStream.rangeClosed(3, limit).boxed()
-				.forEach(n -> {
-					double tmp = (double) n / phi(n);
-					if (tmp > max) {
-						max = tmp;
-						result = n;
+	static void totientiate(int target) {
+		int sqrt = (int) Math.sqrt(target);
+		for (int i = 3; i < target; i += 2) {
+			if (phi[i] == 0) {
+				phi[i] = calculatePhi(i);
+				if (i < sqrt) {
+					int ex = i;
+					for (int e = ex * i; e < target && e > 0; e *= i) {
+						phi[e] = phi[i] * ex;
+						ex = e;
 					}
-				});
+				}
+			}
+		}
+
+		phi[2] = 1;
+		for (int i = 4, j = 2; i <= target; i += 2, j++) {
+			phi[i] = j % 2 == 0 ? 2 * phi[j] : phi[j];
+		}
+
+		for (int i = 2; i <= target; i++) {
+			double localResult = (double) i / phi[i];
+			if (localResult > max) {
+				max = localResult;
+				result = i;
+			}
+		}
 	}
 
-	static long phi(int n) {
-		return IntStream.range(2, n).boxed().filter(m -> PrimeHelper.isCoprime(m, n)).count() + 1;
+	static int calculatePhi(int n) {
+		int phi = n;
+		for (int p = 3; p <= n / p + 1; p += 2) {
+			if (n % p == 0) {
+				phi -= phi / p;
+				do {
+					n /= p;
+				} while (n % p == 0);
+			}
+		}
+		return phi == n ? phi - 1 : n == 1 ? phi : phi - (phi / n);
 	}
 }

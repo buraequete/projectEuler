f65008adfe432b674613836782bcdae84741ab7c
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2017-05-26 14:20:10 +0900

    Q73 improvements

--- src/question/Question73.java
+++ src/question/Question73.java
@@ -15,17 +15,16 @@ public class Question73 {
 	public static void main(String[] args) {
 		TimeHelper.start();
 		int input = 12000;
-		PrimeHelper.fillPrimes(input / 2 + 1);
+		PrimeHelper.fillPrimes((int) Math.sqrt(input) + 1);
 		go(input);
 		ResultHelper.printOut("Result is " + sum);
 		TimeHelper.stop();
 	}
 
 	static void go(int limit) {
-		int count;
-		int startPoint = 4;
+		int count, startPoint = 4;
 		int localMin = min * startPoint, localMax = max * startPoint;
-		for (int i = 4; i <= limit; i++, localMin += min, localMax += max) {
+		for (int i = startPoint; i <= limit; i++, localMin += min, localMax += max) {
 			count = localMin / step + 1;
 			getFactors(i);
 			for (int j = count * step; j < localMax; j += step, count++) {
@@ -47,7 +46,7 @@ public class Question73 {
 
 	static void getFactors(int denum) {
 		factors.clear();
-		List<Integer> primes = PrimeHelper.getPrimes(denum / 2 + 1);
+		List<Integer> primes = PrimeHelper.getPrimes((int) Math.sqrt(denum) + 1);
 		int val = denum;
 		for (int prime : primes) {
 			while (val % prime == 0) {
@@ -55,8 +54,11 @@ public class Question73 {
 				val /= prime;
 			}
 			if (val == 1) {
-				break;
+				return;
 			}
+		}
+		if (val != denum) {
+			factors.add(val);
 		}
 	}
 }
